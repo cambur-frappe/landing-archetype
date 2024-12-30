fb0acf576100dda1aa0751f7bd51bf7415119196
David Pérez <taborlindp@gmail.com>
2024-12-30 19:14:32 +0100

    splash almost configured, copies.ts created!

---
+++ src/content/copies.ts
@@ -0,0 +1,40 @@
+export const copies = {
+  companyName: 'Company Name',
+  h1: 'Hello, World!',
+  hiddenH1: 'Hello, Hidden World!',
+  splashCopy: 'Text satisfying the need of the potential customer, in relation with the headline.',
+  splashAlt: 'Alternative text for the splash image.',
+  subheaing: 'Subheading',
+  subheadingCopy: 'Alternative text satisfying the need of the potential customer, in relation with the heading.',
+  formHeader: 'Form Header',
+  supportingCopy: 'Supporting copy that provides more context to the form.',
+  reinforcingHeader: 'Reinforcing Header',
+  facts: [
+    {
+      title: 'Fact 1',
+      copy: 'Fact 1 copy.'
+    },
+    {
+      title: 'Fact 2',
+      copy: 'Fact 2 copy.'
+    },
+    {
+      title: 'Fact 3',
+      copy: 'Fact 3 copy.'
+    }
+  ],
+  feats: [
+    {
+      title: 'Feature 1',
+      copy: 'Feature 1 copy.'
+    },
+    {
+      title: 'Feature 2',
+      copy: 'Feature 2 copy.'
+    },
+    {
+      title: 'Feature 3',
+      copy: 'Feature 3 copy.'
+    }
+  ]
+};
